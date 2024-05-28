6138037b81680e09543e7931cf2aa6307e23352d
SubramanyaAchar38 <158835042+SubramanyaAchar38@users.noreply.github.com>
2024-05-28 18:46:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+function ispalindrome(str){
+
+    const cleanStr=str.toLowerCase().replace(/[^a-z0-9]/g,'');
+    console.log(cleanStr);
+    const reversedstr=cleanStr.split('').reverse().join('');
+    console.log(reversedstr);
+    return cleanStr===reversedstr;
+
+
+}
+
+function palindromechecker(){
+    const inputText=document.getElementById('inputText');
+    const result=document.getElementById('result');
+    if(ispalindrome(inputText.value)){
+        result.textContent=`"${inputText.value}" is a palindrome`;
+    }else{
+        result.textContent=`"${inputText.value}" is not palindrome`;
+        }
+        result.classList.add('fadeIn');
+        inputText.value='';
+
+    
+}
+
+document.getElementById('checkbutton').addEventListener("click",palindromechecker);
